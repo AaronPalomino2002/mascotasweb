1410f957d2ade92c98d396ead01db1b27b5bd34b
AaronPalomino2002 <aaron_palomino_falcon@hotmail.com>
2024-08-17 02:58:40 -0500

    viaje

--- obj/Debug/net8.0/PROGRA2.AssemblyInfo.cs
+++ obj/Debug/net8.0/PROGRA2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROGRA2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3311151af88c6081cdd8df9feb5a61f73d1803a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROGRA2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROGRA2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
